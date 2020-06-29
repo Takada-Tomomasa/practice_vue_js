048aee737f92e954c64c64fa20c6be0f81d1492f
Takada-Tomomasa <67568084+Takada-Tomomasa@users.noreply.github.com>
2020-06-29 17:30:27 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      todos:
+      [{ "id": 1, "comment": "新しいToDo1", "state": 0 },
+      { "id": 2, "comment": "新しいToDo2", "state": 0 }]
+    }
+  })
