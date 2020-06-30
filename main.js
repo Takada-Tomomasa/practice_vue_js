f0285bd5eb672e62ce4b6274d544d8d354b84f4f
Takada-Tomomasa <67568084+Takada-Tomomasa@users.noreply.github.com>
2020-06-30 11:47:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ var app = new Vue({
     el: '#app',
     data: {
       todos:
-      [{ "id": 1, "comment": "新しいToDo1", "state": 0 },
-      { "id": 2, "comment": "新しいToDo2", "state": 0 }]
+      [{ "id": 1, "name": "画期的なデザインに変更", "contents": "背景色を青に変える", "status": 1},
+      { "id": 2, "name": "ストーリー2", "contents": "内容2", "status": 1},
+      { "id": 3, "name": "ストーリー3", "contents": "内容3", "status": 2},
+      // { "id": 8, "name": "ストーリー8", "contents": "内容8", "status": 2},
+      { "id": 4, "name": "ストーリー4", "contents": "内容4", "status": 3},
+      { "id": 5, "name": "ストーリー5", "contents": "内容5", "status": 3},
+      { "id": 6, "name": "ストーリー6", "contents": "内容6", "status": 4},
+      { "id": 7, "name": "ストーリー7", "contents": "内容7", "status": 4}]
+    },
+    compute: {
+      showTodos:function(){
+        
+      }
     }
   })
