9f6ff5d6064cc1d4e7854c2eb6f554f97f4c0c7d
Takada-Tomomasa <67568084+Takada-Tomomasa@users.noreply.github.com>
2020-07-01 15:48:48 +0900

    Add files via upload
    詳細画面周り、削除ボタン実装

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ var app = new Vue({
     el: '#app',
     data: {
       showContent: false,
-      addStoryName: '',
+      showDetails: false,
+      // addStoryName: '',
+      // addStoryContents: '',
+      // storyName: '',
+      // storyContents: '',
+      // selected: '',
+      // changeID: '',
+      storyCount: 100,
+      options: [
+        { id: 1, name: 'Todo' },
+        { id: 2, name: 'Doing' },
+        { id: 3, name: 'Review' },
+        { id: 4, name: 'Done'}
+      ],
       todos:
       [{ id: 1, name: "画期的なデザインに変更", contents: "背景色を青に変える", status: 1},
       { id: 2, name: "ストーリー2", contents: "内容2", status: 1},
       { id: 3, name: "ストーリー3", contents: "内容3", status: 2},
-      // { id: 8, name: "ストーリー8", contents: "内容8", status: 2},
       { id: 4, name: "ストーリー4", contents: "内容4", status: 3},
       { id: 5, name: "ストーリー5", contents: "内容5", status: 3},
       { id: 6, name: "ストーリー6", contents: "内容6", status: 4},
@@ -20,15 +32,42 @@ var app = new Vue({
       closeModal: function(){
         this.showContent = false
       },
+      openModalDetails: function(item){
+        this.showDetails = true
+        this.storyName = item.name
+        this.storyContents = item.contents
+        this.changeID = item.id
+        this.selected = item.status
+      },
+      closeModalDetails: function(){
+        this.showDetails = false
+      },
       addItem: function(){
         this.todos.push({
-          id: 10,
+          id: this.storyCount,
           name: this.addStoryName,
-          contents: "内容10",
+          contents: this.addStoryContents,
           status: 1
         })
         this.showContent = false
         this.addStoryName = ''
+        this.addStoryContents = ''
+        this.storyCount = this.storyCount + 1
+      },
+      changeItem: function(){
+        index = this.todos.findIndex((todo) => todo.id === this.changeID)
+        this.todos.splice(index,1,{
+          id: this.changeID,
+          name: this.storyName,
+          contents: this.storyContents,
+          status: this.selected
+        })
+        this.showDetails = false
+      },
+      removeItem: function(){
+        index = this.todos.findIndex((todo) => todo.id === this.changeID)
+        this.todos.splice(index,1)
+        this.showDetails = false
       }
     }
   })
