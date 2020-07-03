7e104cdab10f8c286807eb84efac9a6ccc0915f9
Takada-Tomomasa <67568084+Takada-Tomomasa@users.noreply.github.com>
2020-07-03 13:18:12 +0900

    Add files via upload

---
+++ src/components/Kanban.vue
@@ -0,0 +1,79 @@
+<template>
+  <table class="kanban">
+    <tr>
+      <td>
+        <table class="todo">
+          <thead>
+            <tr>
+              <th>ToDo</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>test</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>
+                <button>✙</button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </td>
+
+      <td>
+        <table class="doing">
+          <thead>
+            <tr>
+              <th>Doing</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>test</td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+
+      <td>
+        <table class="review">
+          <thead>
+            <tr>
+              <th>Review</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>test</td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+
+      <td>
+        <table class="done">
+          <thead>
+            <tr>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>test</td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="mainpage">
+    <h2>かんばんボード</h2>
+    <p>このボードは、テクコン部新人3名によるVue.jsの演習成果物である。</p>
+    <Kanban />
+  </div>
+</template>
+<script>
+import Kanban from "./Kanban";
+export default {
+  name: "MainPage",
+  components: {
+    Kanban
+  }
+};
+</script>
+<style scoped>
+#mainpage {
+  text-align: left;
+}
+</style>
