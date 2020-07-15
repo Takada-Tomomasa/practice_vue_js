432d92758e556e8a1d5fe231e010a8bd93472aca
Takada-Tomomasa <67568084+Takada-Tomomasa@users.noreply.github.com>
2020-07-15 17:19:43 +0900

    Add files via upload

--- tests/unit/MainPage.spec.js
+++ tests/unit/MainPage.spec.js
@@ -1,23 +1,17 @@
 import { shallowMount } from "@vue/test-utils";
 import MainPage from "@/components/MainPage.vue";
 
-let wrapper;
-
-beforeEach(() => {
-  wrapper = shallowMount(MainPage);
-});
-afterEach(() => {
-  wrapper.destroy();
-});
-
 describe("MainPage.vue", () => {
   it("test Mainpage h2", () => {
+    const wrapper = shallowMount(MainPage);
     expect(wrapper.findAll("h2").at(0).text()).toEqual("かんばんボード");
   }),
     it("test Mainpage p", () => {
+      const wrapper = shallowMount(MainPage);
       expect(wrapper.find("p").text()).toEqual("とれぷろ！ 作");
     }),
     it("test Mainpage kanban", () => {
+      const wrapper = shallowMount(MainPage);
       const Kanban = wrapper.find("Kanban-stub");
       expect(Kanban.exists()).toBeTruthy();
     });
